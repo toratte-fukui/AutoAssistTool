e87d6b5e161c03b673fb997a97ffdd91796e45ac
toratte_fukui <toratte.fukui@gmail.com>
2024-01-20 21:07:49 +0900

    拡張機能の設定追加

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from lib.scraping_tool import ScrapingTool
 from selenium.webdriver.common.by import By
 
-tool = ScrapingTool(
-  url="https://google.com",
-)
-# list_details = tool.driver.find_elements(By.CLASS_NAME, "iw-list-detail")
-# print(list_details)
+from lib.scraping_tool import ScrapingTool
+
+tool = ScrapingTool(url="https://google.com")
+
+list_details = tool.driver.find_elements(By.CLASS_NAME, "iw-list-detail")
+print(list_details)
